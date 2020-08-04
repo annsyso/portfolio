1a3b48c1713551f16d595a82ad8955660ffa15a7
annsyso <anniesyso@gmail.com>
2020-08-04 12:36:38 -0400

    added mobile navigation styling and js interactivity

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const mainMenu = document.querySelector('.main-menu');
+const closedMenu = document.querySelector('.closed-menu');
+const openMenu = document.querySelector('.open-menu');
+
+openMenu.addEventListener('click', show);
+closedMenu.addEventListener('click', close);
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+
+function close() {
+    console.log('works');
+    mainMenu.style.top = '-100%';
+}
+
+console.log('works');
